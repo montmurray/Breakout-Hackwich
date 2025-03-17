6c22a81efc96ff79aef46eee1f2ea6aaba018337
montmurray <vinylscratch1103@gmail.com>
2025-03-17 09:51:23 -0500

    created ball object

--- Breakout Hackwich Shared/GameScene.swift
+++ Breakout Hackwich Shared/GameScene.swift
@@ -9,11 +9,18 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var ball = SKShapeNode()
     
     override func didMove(to view: SKView) {
         createBackground()
+        resetGame()
         // does stuff when app opens
     }
+    
+    func resetGame() {
+        makeBall()
+    }
+    
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
         for i in 0...1 {
@@ -28,4 +35,24 @@ class GameScene: SKScene {
             starsBackground.run(moveForever)
         }
     }
-}
+    
+    func makeBall() {
+        ball.removeFromParent()
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        ball.physicsBody?.isDynamic = false
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        ball.physicsBody?.friction = 0
+        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.restitution = 1
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        
+        addChild(ball)
+    }
+    }
